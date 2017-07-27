de30f70d86a24b922736dac0fe56ada73e391fcd
flight2k <frederic@ordi-logic.com>
2017-07-27 19:59:05 -0400

    Update neviweb.js

--- neviweb/neviweb.js
+++ neviweb/neviweb.js
@@ -1,1 +1,30 @@
+module.exports = function(RED) {
 
+  function NeviwebAccountNode(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    var url = config.url;
+    var email = this.credentials.email;
+    var password = this.credentials.password;
+    var request = require("request");
+    
+    this.doRequest = function(options, callback) {
+      options.email = email;
+      options.password = password;
+      
+  }
+  
+  RED.nodes.registerType("neviweb-account", NeviwebAccountNode, {
+    credentials: {
+      email: {
+        type: "text"
+      },
+      password: {
+        type: "password"
+      }
+    }
+  });
+  
+  RED.nodes.registerType("neviweb-gateway", NeviwebGatewayNode);
+  
+}
