e9f534413c3b11f73f34870637d7dd8a80262778
flight2k <frederic@ordi-logic.com>
2017-07-30 14:38:38 -0400

    modification de la requête GET device

--- neviweb/neviweb.js
+++ neviweb/neviweb.js
@@ -69,7 +69,7 @@ module.exports = function(RED) {
     this.device = function(msg, callback) {
       var options = {
         rejectUnauthorized: false,
-        uri: decodeURIComponent(url + 'device'),
+        uri: decodeURIComponent(url + 'device?gatewayID=' + msg.gateway),
         method: "GET",
         headers: {},
         body: {
